f26187680cd2b29957ab4e907fa9ffadfd2178cf
jcalvin4 <joe.m.calvin@outlook.com>
2024-10-09 19:01:25 -0700

    bb

---
+++ bb/node-bulletin-board/bulletin-board-app/jest.config.js
@@ -0,0 +1,11 @@
+// For every .js file in the src directory, require at least 80% line coverage.
+module.exports = {
+    coverageThreshold: {
+        './src/*.js': {
+            lines: 80
+        }
+    }
+}
+//
+//
+//
